e65637dcc8950be2a88413f7a95b0b38d3503d6e
capossele <angelo@Angelo.local>
2019-03-14 15:44:18 +0000

    README updated

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func runSimulation(b Benchmark, tsa string, lambda, alpha float64) Result {
 		//drawTangleMode = 4: Tangle with highlighted path of random walker transitioning to first approver
 		//drawTangleMode = 5: Tangle with highlighted path of random walker transitioning to last approver
 		//drawTangleMode = -1: 10 random walk and draws the Tangle at each step (for GIF or video only)
-		drawTangleMode:        -1,
+		drawTangleMode:        1,
 		horizontalOrientation: true,
 	}
 
